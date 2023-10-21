eea89252dc7118eae381259ed38fd99356373f14
Mavgician <jonjon0524@gmail.com>
2023-10-21 19:22:07 +0800

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+function Home() {
+  return (
+    <main>
+      {
+      /*
+        Lagay niyo dito pinaka layout ng home page for the store. Naka integrate ung bootstrap dito so all goods tayo dun.
+        Ang hindi lang nakaintegrate sa root component is ung javascript ng bootstrap. So kung mag cacarousel or something,
+        sabihan niyo ko, kasi medj mahirap iexplain kung paano i allow un - 
+
+        Small explanation:
+          Next.js is mostly server-side rendered, ang pag import ng javascript is not "server" friendly. Need nating i-convert
+          yung component into a client component para maimport ung javascript. This is actually better para forced tayo gumawa
+          ng components for various things on a page. Slideshow component na lalagyan lang ng picture array etc. etc.
+        
+        Watch niyo rin ung sinend ko sa GC natin para mafamiliarize kayo sa framework. I suggest video number 3 para magets niyo
+        ung routing ng pages. Kung paano siya nahahandle ng server ni next.js.
+
+        PAMPADALI TO NG BUHAY NATIN OKAY >:(
+
+        -Mavs ang leader ng bayan
+      */
+      }
+      <p>test</p>
+    </main>
+  )
+}
+
+export default Home
