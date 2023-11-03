2f46fa7058cc030a81abf6b827013b279085833e
Mavgician <jonjon0524@gmail.com>
2023-11-03 12:59:01 +0800

    Ported home page and added a template page

---
+++ src/app/dev/template/page.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../../firebase.js'
+
+
+function Home() {
+  async function firestore() {
+    await addDoc(collection(db, 'test'), {
+      name: "some random string",
+      int: 123
+    })
+  }
+
+  return (
+    <main>
+      {
+      /*
+        Lagay niyo dito pinaka layout ng home page for the store. Naka integrate ung bootstrap dito so all goods tayo dun.
+        Ang hindi lang nakaintegrate sa root component is ung javascript ng bootstrap. So kung mag cacarousel or something,
+        sabihan niyo ko, kasi medj mahirap iexplain kung paano i allow un - 
+
+        Small explanation:
+          Next.js is mostly server-side rendered, ang pag import ng javascript is not "server" friendly. Need nating i-convert
+          yung component into a client component para maimport ung javascript. This is actually better para forced tayo gumawa
+          ng components for various things on a page. Slideshow component na lalagyan lang ng picture array etc. etc.
+        
+        Watch niyo rin ung sinend ko sa GC natin para mafamiliarize kayo sa framework. I suggest video number 3 para magets niyo
+        ung routing ng pages. Kung paano siya nahahandle ng server ni next.js.
+
+        PAMPADALI TO NG BUHAY NATIN OKAY >:(
+
+        -Mavs ang leader ng bayan
+      */
+      }
+      <h1>You have reached the template page!</h1>
+    </main>
+  )
+}
+
+export default Home
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,39 +1,126 @@
-'use client'
-
-import { collection, addDoc } from 'firebase/firestore'
-import { db } from './firebase'
+import './globals.css'
 
 function Home() {
-  async function firestore() {
-    await addDoc(collection(db, 'test'), {
-      name: "some random string",
-      int: 123
-    })
-  }
-
   return (
     <main>
-      {
-      /*
-        Lagay niyo dito pinaka layout ng home page for the store. Naka integrate ung bootstrap dito so all goods tayo dun.
-        Ang hindi lang nakaintegrate sa root component is ung javascript ng bootstrap. So kung mag cacarousel or something,
-        sabihan niyo ko, kasi medj mahirap iexplain kung paano i allow un - 
-
-        Small explanation:
-          Next.js is mostly server-side rendered, ang pag import ng javascript is not "server" friendly. Need nating i-convert
-          yung component into a client component para maimport ung javascript. This is actually better para forced tayo gumawa
-          ng components for various things on a page. Slideshow component na lalagyan lang ng picture array etc. etc.
-        
-        Watch niyo rin ung sinend ko sa GC natin para mafamiliarize kayo sa framework. I suggest video number 3 para magets niyo
-        ung routing ng pages. Kung paano siya nahahandle ng server ni next.js.
-
-        PAMPADALI TO NG BUHAY NATIN OKAY >:(
-
-        -Mavs ang leader ng bayan
-      */
-      }
-      <p>test</p>
-      <button onClick={firestore}>Database Test</button>
+      <nav class="navbar navbar-expand-md">
+        <a class="navbar-brand" href="#">
+          <img src="./favicon.ico" alt="Silkroad" width="60" height="60" />
+        </a>
+        <button
+          class="navbar-toggler navbar-dark"
+          type="button"
+          data-toggle="collapse"
+          data-target="#main-navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navigation">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Mens</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Womens</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              class="d-block w-100"
+              src="/43453.jpg"
+              alt="First slide"
+              width="100"
+              height="800"
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Crew Neck Short Sleeve T-shirt</h5>
+              <p>Gift him with all the colors for his wardrobe</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              class="d-block w-100"
+              src="/wad.jpg"
+              alt="Second slide"
+              width="100"
+              height="800"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              class="d-block w-100"
+              src="/htfhtf.jpg"
+              alt="Third slide"
+              width="100"
+              height="800"
+            />
+          </div>
+        </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <footer class="page-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12">
+              <h6 class="text-uppercase font-weight-bold">About Us</h6>
+              <p>
+                Our store was founded in 1860 by two best friends, John Henry R.
+                Relucio and John Mark B. Ormido of New Haven, Connecticut. They
+                have always had a passion for fashion and clothing design. When
+                they shared a dorm room in college, Henry and John Mark began
+                crafting their brand, image, and concept for their first clothing
+                store they would be opening. They didn’t want to limit their
+                concept to just young, trendy styles.
+              </p>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <h6 class="text-uppercase font-weight-bold">Contact</h6>
+              <p>
+                Aurora Blvd., Cubao, Quezon City, <br />Silkroad@silk.com
+                <br />+63 905 456 4546
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright text-center">2023 Copyright</div>
+      </footer>
     </main>
   )
 }
